f504d9b5f426b6f80d04e46fcee005f918e9e0e4
apple <apple@apples-MacBook-Air-2.local>
2022-02-24 16:21:40 +0530

    added function to compute the max parentcount

--- index.js
+++ index.js
@@ -2,15 +2,82 @@ const axios = require('axios');
 const to = require("await-to-js");
 const Services = require("./services/services");
 
-async function compute(blockHeight) {
+// returns list of all transactions
+async function computeTransactions(blockHeight) {
 
     const [, blockHash] = await Services.getBlockHashFromBlockHeight(blockHeight)
     if(blockHash) {
-        const [, data] = await Services.getBlockTransactions(blockHash, 26);
+        const txs = await Services.getBlockAllTransactions(blockHash);
+        return [null, txs]
     } else {
-
+        return ["error", null];
     }
+
+}
+
+// TODO: no global
+let traversedTransactions = {};
+
+async function findMaxAncestorySetsTransactions(num) {
+    const [err, txs] = await computeTransactions(680000);
+    /*
+        [tx]
+        tx: {
+            txid,
+            vin: [
+                       {
+                        "txid"
+                       }
+                  ],
+            vout: [] // na
+        }
+     */
+
+    // txs.map(tx => )
+
+    txs.map(({txid, vin}) => {
+        traversedTransactions[txid] = {
+            done: false,
+            parentCount: 0,
+            vin
+        };
+    })
+
+    // recursively traverse all transactions
+    for(let i=0; i<txs.length; i++) {
+        let tx = txs[i];
+        if(!traversedTransactions[tx.txid].done) { // if haven't traversed
+            traverse(tx.txid);
+        }
+    }
+
+    // find max 10 transactions
+
+    traversedTransactions.sort((a, b) => {
+        return a.parentCount - b.parentCount
+    })
+
+    return traversedTransactions.slice(0, num);
+}
+
+// traverse a transaction, and return the parent count, 0 for the root node
+function traverse(txid) {
+
+    const tx = traversedTransactions[txid];
+
+    // find all the parents and push them to traverse
+    const toTraverseParents = tx.vin.filter(({txid}) => traversedTransactions[txid].done === false)
+    let maxpc = tx.parentCount;
+
+    toTraverseParents.map(({txid}) => {
+        let pc = traverse(txid);
+        maxpc = Math.max(pc, maxpc);
+    });
+
+    tx.parentCount = maxpc;
+
+    return tx.parentCount;
 }
 
 
-compute(680000).then(console.log).catch(console.log);
+findMaxAncestorySetsTransactions(10).then(console.log).catch(console.log);
--- services/services.js
+++ services/services.js
@@ -23,8 +23,45 @@ class Services {
     }
 
     static async getBlockTransactions(hash, startIndex) {
-        const [err, data] = await to(axios.get(`${this.url}/block/${hash}/txs/[${startIndex}]`));
-        return [err, data.data];
+        const [err, data] = await to(axios.get(`${this.url}/block/${hash}/txs/${startIndex}`));
+        if(data.data) {
+            const txs = [];
+            data.data.map(({txid, vin}) => {
+                const _vin = vin.map(x => x.txid);
+                txs.push(
+                    {
+                        txid, vin: _vin
+                    }
+                )
+            })
+
+            return [null, txs];
+        }
+        return [err, null];
+    }
+
+    static async getBlockAllTransactions(hash) {
+        const txs = [];
+        let index = 0;
+        let hasNext = true;
+
+        while(hasNext) {
+            const [, data] = await this.getBlockTransactions(hash, index);
+            if (data) {
+                txs.push(...data);
+                if (data.length === 25) {
+                    // make more call
+                    index += data.length;
+                } else {
+                    hasNext = false;
+                }
+            }
+            if(txs.length > 200) {
+                return txs;
+            }
+        }
+
+        return txs;
     }
 }
 
